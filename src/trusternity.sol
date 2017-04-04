681cd4a824d934b40622ccbab3cf95614b7754e7
Long Nguyen <longnguyenhoang@outlook.com>
2017-04-04 15:37:49 +0200

    Change arbitrary byte array to byte32
    Add GET function

--- src/trusternity.sol
+++ src/trusternity.sol
@@ -4,26 +4,37 @@ contract Trusternity
 {	
 	//An Identity Provider struct contains a provider name, 
 	//a public key fingerprint and a List of STR	
-	struct IdentityProvider{
-		string Name;
-		bytes Fingerprint;
-		mapping(uint => bytes) RootList;
+	struct Provider{		
+		string name;
+		bytes32 fingerprint;		
+		mapping(uint64 => bytes32) rootList;
+	}	
+
+	//List of all Provider
+	mapping(address => Provider) public ProviderList;	
+
+	//An Identity Provider registers itself to the system by providing 
+	//its name and its key fingerprint.	
+	function Register(string _name, bytes32 _fprint){
+		ProviderList[msg.sender].name = _name;
+		ProviderList[msg.sender].fingerprint = _fprint;	
 	}
 
-	//List of all IdentityProvider
-	mapping(address => IdentityProvider) public IdentityProviderList;
-	
-	//An IdentityProvider registers itself to the system by providing 
-	//its name and its key fingerprint.
-	//TODO: Make sure the combination is unique	
-	function Register(string identityProviderName, bytes fingerprint){
-		IdentityProviderList[msg.sender].Name = identityProviderName;
-		IdentityProviderList[msg.sender].Fingerprint = fingerprint;
-	}
-
-	//An IdentityProvider publishes the STR every epoch.
+	//An Provider publishes the STR every epoch.
 	//The STR is mapped to its timeStamp 
-	function Publish(uint timeStamp, bytes STR) {
-		IdentityProviderList[msg.sender].RootList[timeStamp] = STR;		
-	}              	
+	function Publish(uint64 epoch, bytes32 STR) {		
+		ProviderList[msg.sender].rootList[epoch] = STR;		
+	}
+
+	function GetProviderName(address _ads) returns (string){
+		return ProviderList[_ads].name;
+	}
+
+	function GetProviderFingerprint(address _ads) returns (bytes32){
+		return ProviderList[_ads].fingerprint;
+	}
+
+	function GetSTR(address _ads, uint64 epoch) returns (bytes32){
+		return ProviderList[_ads].rootList[epoch];
+	}
 }
